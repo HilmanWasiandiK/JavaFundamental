34cfb97f7e2be594df694f9a88b5f760ecacda09
HilmanWasiandiK <hilman.wasiandi.tif20@polban.ac.id>
2021-12-06 03:05:18 +0700

    semua commit minggu 11

--- src/TugasPraktikum10/Pembagian.java
+++ src/TugasPraktikum10/Pembagian.java
@@ -2,6 +2,6 @@ package TugasPraktikum10;
 
 public class Pembagian {
 	public static void main(String[] args) {
-		System.out.println(5/0);
+		System.out.println("hasil pembagian 5 dibagi 0 adalah " + 5/0);
 	}
 }
